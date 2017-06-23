f6fec81e564fc1a8f0cdd69404b8e144553b4fb4
Soup-Flies <benjcharais@gmail.com>
2017-06-22 19:34:37 -0600

    Add files via upload

---
+++ assets/js/logic.js
@@ -0,0 +1,45 @@
+var zillowApi = "http://www.zillow.com/webservice/GetZestimate.htm";
+var zillowKey = "X1-ZWz195aafxhlor_4vl2o";
+var googlePlacesKey = "AIzaSyBQCnwzPy31r3t741_zCN9LCy81753WDzw";
+var googleKey = "AIzaSyAWE8SJk1mkR4Jlubw5Q5DoVepI2eIdh1I";
+
+var apiUrl = zillowApi + "?zws-id=" + zillowKey;
+$.ajax({
+
+  method: "GET",
+  url: apiUrl,
+  headers: {
+    "Accept": "application/json"
+  }
+})
+  .done(function(data) {
+    console.log("Hello!");
+    console.log(data);
+  })
+
+
+
+var apiUrl = "https://maps.googleapis.com/maps/api/js?key=" + googlePlacesKey + "&libraries=places&callback=initMap";
+  $.ajax({
+    method: "GET",
+    url: apiUrl,
+    headers: {
+      "Accept": "application/json"
+    }
+  })
+    .done(function(data) {
+      console.log("Hello!");
+      console.log(data);
+    })
+
+var apiUrl = "https://maps.googleapis.com/maps/api/js?key=" + googleKey + "&callback=initMap";
+$.ajax({
+  method: "GET",
+  url: apiUrl,
+  headers: {
+    "Accept": "application/json"
+  }
+})
+  .done(function(data) {
+    console.log(data);
+  })
